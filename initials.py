8345e73c352ce8a1baad637b190930b2361bc7d3
benjaminsquire <2425403875@live.gctu.edu.gh>
2026-06-01 06:17:33 +1200

    completed initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
 
 def main():
   pass #code goes here
+# File: initials.py
+# Description: Print out my initials in large letters.
+# Assignment Number: 1
+#
+# Name: Benjamin Squire
+# STUDENT ID: 2425403875
+# Email: 2425403875@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Benjamin Squire, this programming assignment is my own work
+# and I have not provided this code ot any other student.
 
+
+def main():
+  # Print my initials using large block letters.
+
+  print()
+
+  print("...BSB")
+
+  print()
+
+print("...BBBBBBBBBBBB...****...SSSSSSSSSSSS...****...BBBBBBBBBBBB...****")
+print("...BBBBBBBBBBBB...****...SSSSSSSSSSSS...****...BBBBBBBBBBBB...****")
+print("...BB........BB...****...SSSS...........****...BB........BB...****")
+print("...BB........BB...****...SSSS...........****...BB........BB...****")
+print("...BBBBBBBBBBBB...****...SSSSSSSS.......****...BBBBBBBBBBBB...****")
+print("...BBBBBBBBBBBB...****.......SSSSSS.....****...BBBBBBBBBBBB...****")
+print("...BB........BB...****...........SSSS...****...BB........BB...****")
+print("...BB........BB...****...........SSSS...****...BB........BB...****")
+print("...BBBBBBBBBBBB...****...SSSSSSSSSSSS...****...BBBBBBBBBBBB...****")
+print("...BBBBBBBBBBBB...****...SSSSSSSSSSSS...****...BBBBBBBBBBBB...****")
+main()
 
 
 main()
